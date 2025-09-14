72b107183061869659882cda5053fbc7acde0fe6
ichul <ikizaki01@gmail.com>
2025-09-14 16:30:41 +0700

    ichul

--- src/kelomp5/icul.java
+++ src/kelomp5/icul.java
@@ -124,6 +124,7 @@ public class icul extends javax.swing.JFrame {
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         // TODO add your handling code here:
+        dispose();
     }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
