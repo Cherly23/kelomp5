1a8fd35b3bbe5e8311b6b1317c7cea26dc858aa5
fatoni883 <mimamfatoni2@gmail.com>
2025-09-14 16:04:01 +0700

    Toni3 commit3

--- src/ToniJFrame.java
+++ src/ToniJFrame.java
@@ -105,6 +105,7 @@ public class ToniJFrame extends javax.swing.JFrame {
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         // TODO add your handling code here:
+        dispose();
     }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
